1340971cb2c98a063c7c2cf10c900860fd0f69a7
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-09-18 15:26:20 -0400

    Switched over from Play's heavy functional syntax to a for-comprehension - https://www.lucidchart.com/techblog/2016/08/29/speeding-up-restful-services-in-play-framework/

--- app/models/dto/CreatePerson.scala
+++ app/models/dto/CreatePerson.scala
@@ -5,27 +5,28 @@ import java.util.UUID
 import models.domain.{Person => PersonModel}
 import models.dto.Gender.Gender
 import play.api.data.validation.ValidationError
-import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 case class CreatePerson(firstName: String, lastName: String, studentId: String, gender: Gender)
 
 object CreatePerson {
   implicit val jsonWrites = Json.writes[CreatePerson]
-  implicit val jsonValidatedReads = (
-    (JsPath \ "firstName").read[String] //vanilla read followed by additional validators
-      .filter(ValidationError("must be more than 2 characters"))(fname => fname.length > 2) and
+  implicit val jsonValidatedReads = Reads[CreatePerson] {
+    json =>
+      for {
+        firstName <- (json \ "firstName").validate[String]
+          .filter(JsError(ValidationError("must be more than 2 characters")))(fname => fname.length > 2)
 
-      (JsPath \ "lastName").read[String]
-        .filter(ValidationError("must be more than 2 characters"))(lname => lname.length > 2) and
+        lastName <- (json \ "lastName").validate[String]
+          .filter(JsError(ValidationError("must be more than 2 characters")))(lname => lname.length > 2)
 
-      (JsPath \ "studentId").read[String]
-        .filter(ValidationError("must be 10 digits"))(number => number.length == 10)
-        .filter(ValidationError("must be a number"))(number => number.forall(Character.isDigit)) and
+        studentId <- (json \ "studentId").validate[String]
+          .filter(JsError(ValidationError("must be a number")))(number => number.forall(Character.isDigit))
+          .filter(JsError(ValidationError("must be 10 digits")))(number => number.length == 10)
 
-      (JsPath \ "gender").read[Gender]
-
-    ) (CreatePerson.apply _)
+        gender <- (json \ "gender").validate[Gender]
+      } yield CreatePerson(firstName, lastName, studentId, gender)
+  }
 
   implicit class CreatePersonOps(request: CreatePerson) {
     def toModel: PersonModel =
